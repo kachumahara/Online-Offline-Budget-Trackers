eaff405e047a90fdfd01b36179cb4be52fb7956b
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-03-31 00:18:50 -0700

    evt function

--- public/service-worker.js
+++ public/service-worker.js
@@ -69,16 +69,16 @@ self.addEventListener("fetch", function(evt) {
     return;
   }
 
-  // evt.respondWith(
-  //   caches.match(evt.request).then(function(response) {
-  //     return response || fetch(evt.request);
-  //   })
-  // );
   evt.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-            return response || fetch(evt.request);
-        });
+    caches.match(evt.request).then(function(response) {
+      return response || fetch(evt.request);
     })
-);
+  );
+//   evt.respondWith(
+//     caches.open(CACHE_NAME).then(cache => {
+//         return cache.match(evt.request).then(response => {
+//             return response || fetch(evt.request);
+//         });
+//     })
+// );
 });
